4ec4abcce127b3bada39d59787a25e7fcc03e352
ognjen <ognjenkl@gmail.com>
2016-06-21 00:40:01 +0200

    Init commit

---
+++ SecureChatOKServer/src/ChatServer.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+//import java.util.concurrent.CopyOnWriteArrayList;
+
+
+public class ChatServer {
+	
+	int serverPort = 1234;
+	ServerSocket sSocket;
+	Socket socket;
+	boolean listening = true;
+	//List<ChatServerThread> listThreads = new CopyOnWriteArrayList<ChatServerThread>();
+	Map<String, ChatServerThread> mapThreads = new ConcurrentHashMap<String, ChatServerThread>();
+	
+	public void start(){
+		try {
+			System.out.println("Server started at port "+serverPort);
+			sSocket = new ServerSocket(serverPort);
+			while(listening){
+				
+				socket = sSocket.accept();
+				
+				ChatServerThread cst = new ChatServerThread(socket, mapThreads);
+				cst.start();
+				//listThreads.add(cst);
+				//showAllClients();
+			}
+				
+			sSocket.close();
+		
+		} catch (IOException e) {
+			try {
+				socket.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		ChatServer cs = new ChatServer();
+		cs.start();
+
+	}
+
+}
---
+++ SecureChatOKServer/src/ChatServerThread.java
@@ -0,0 +1,133 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ChatServerThread extends Thread{
+
+	Socket socket;
+	BufferedReader in;
+	PrintWriter out;
+	Map<String, ChatServerThread> mapThreads;
+	String userOfThread;
+
+	String request;
+	
+	public ChatServerThread(Socket socket, Map<String, ChatServerThread> mapThreads){
+		this.socket = socket;
+		this.mapThreads = mapThreads;
+	}
+	
+	public void run(){
+		try {
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+			
+			
+			while ((request = in.readLine()) != null){
+				//json {"data":"og","from":"og","to":"s","type":"login"}
+				System.out.println("Dobio server: "+request);
+				JSONObject jsonObject = new JSONObject(request);
+				String to	 = jsonObject.getString("to");
+				String from	 = jsonObject.getString("from");
+				String type	 = jsonObject.getString("type");
+				String data	 = jsonObject.getString("data");
+
+				if(type.equals("login")){
+					//edit, authentication needed
+					if(mapThreads.get(from) != null){
+						out.println("login failed");
+						continue;
+					}
+					mapThreads.put(from, this);
+					userOfThread = from;
+					showAllClients();
+					String clients = getAllClients();
+					System.out.println("Svi kljenti na serveru: " + clients);
+					out.println(clients);
+					notifyAllThreadsAboutUserChange();
+				} else if (type.equals("chat")){
+					if(mapThreads.get(to) != null)
+						mapThreads.get(to).sendMessageAsJson(to, from, type, data);
+					else
+						mapThreads.get(from).sendMessageAsJson(from, to, "server", "Korisnik \""+to+"\" se odjavio!");
+				}else{
+					System.out.println("Someting is wrong");
+				}
+				
+			}//end while		
+			
+			
+		} catch (IOException | JSONException e) {	
+			System.out.println("IOException: java.net.SocketException: Connection reset");
+		} finally {
+			try {
+				mapThreads.remove(userOfThread);
+				notifyAllThreadsAboutUserChange();
+				System.out.println(getAllClients());
+				in.close();
+				out.close();
+				socket.close();
+			} catch (IOException e) {
+				System.out.println("Finally catch");
+				e.printStackTrace();
+			}
+			
+		}
+	}
+
+	
+	public void showAllClients(){
+		System.out.println("All clients:");
+		for(Map.Entry<String, ChatServerThread> entry : mapThreads.entrySet()){
+			System.out.println(entry.getKey());
+		}
+	}
+	
+	public String getAllClients(){
+		String clients = "";
+		for(Map.Entry<String, ChatServerThread> entry : mapThreads.entrySet()){
+			clients += entry.getKey()+";";
+		}
+		return clients;
+	}
+	
+	public void sendMessageAsJson(String to, String from, String type, String data){
+		JSONObject jsonObj = new JSONObject();
+		try {
+			jsonObj.put("to", to);
+			jsonObj.put("from", from);
+			jsonObj.put("type", type);
+			jsonObj.put("data", data);
+			//System.out.println("print raw json: " + jsonObj);
+			out.println(jsonObj);
+
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public void notifyAllThreadsAboutUserChange(){
+		String clients = getAllClients();
+		for(Map.Entry<String, ChatServerThread> entry : mapThreads.entrySet()){
+			//json {"data":"og;ir;dr;","from":"s","to":"og","type":"updateUsers"}
+			
+			String to = entry.getKey();
+			String from = "s";
+			String type = "updateUsers";
+			String data = clients;
+			
+			entry.getValue().sendMessageAsJson(to, from, type, data);
+		}
+	}
+
+}
